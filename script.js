edfaf3c75c4beb1d2f2d52ec032e594bcec52cff
Juan Coret <coret.juan@gmail.com>
2018-07-25 13:21:40 +0200

    made contact form useable

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ $(function() {
 $(window).scroll(function(){
     $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250); 
   });
+
+let mainNav=document.getElementById('main-nav');
+let navbarToggle=document.getElementById('navbar-toggle');
+
+navbarToggle.addEventListener('click',function(){
+
+    if(this.classList.contains('active')){
+        mainNav.style.display="none";
+        this.classList.remove('active');
+    }
+    else{
+        mainNav.style.display="flex";
+        this.classList.add('active');
+
+    }
+});
